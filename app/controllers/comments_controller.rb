48f8781fa0187042ef47d1149635a47e3fdc6943
Lucas <lucasstos.oliveira@gmail.com>
2016-01-17 00:55:28 -0200

    paginacao

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
 
 	def index
-		@comments= Comment.all
+		@comments= Comment.all.paginate(:page => params[:page], :per_page => 5)
+	end
+
+	def create(comment)
+
 	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
 
 	has_many :comment_children, foreign_key: :created_by_comment_id, class_name: "Comment"
+	
 end
